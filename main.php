ae5c23b2716364a1f0acd36ed11d4d00ecf03bfe
Benjamin Benesh <benesh9@gmail.com>
2013-11-27 14:31:38 -0600

    Refactors into a class.

--- main.php
+++ main.php
@@ -1,45 +1,56 @@
 <?php
 require_once('vendor/autoload.php');
-date_default_timezone_set('UTC');
 
-/*
- * CONFIG
- */
-$hipchat_token = '';
-$hipchat_room = '';
 
-/*
- * Init
- */
-$hc = new \HipChat\HipChat($hipchat_token);
-$reader = new \Sabre\XML\Reader();
-$current_playing = '';
-
-/*
- * Daemon.  Updates every 30 seconds.
- */
-while(1)
+class SiriusNowPlaying
 {
-    $currentTime = date('m-d-G:i:00');
-    $reader->open('http://www.siriusxm.com/metadata/pdt/en-us/xml/channels/hardattack/timestamp/'.$currentTime);
-    $output = $reader->parse();
+    protected $hipchat_room;
+    protected $hc;
+    protected $reader;
+    protected $current_playing;
 
-    $song_title = $output['value'][2]['value'][3]['value'][6]['value'][15]['value'];
-    $song_artist = $output['value'][2]['value'][3]['value'][0]['value'][1]['value'];
-    $now_playing = "Now Playing: $song_title by $song_artist";
-
-    if($current_playing == $now_playing)
+    public function __construct($hipchat_token, $hipchat_room)
     {
-        // Song hasn't changed.  Do nothing.  Log cycle.
-        echo '.';
-    } else
-    {
-        echo PHP_EOL;
-        // New song.  Update current_playing and send alert.
-        $current_playing = $now_playing;
-        $hc->message_room($hipchat_room, 'SiriusXM', $current_playing);
-        echo $current_playing . PHP_EOL;
+        date_default_timezone_set('UTC');
+        $this->hipchat_room = $hipchat_room;
+        $this->hc = new \HipChat\HipChat($hipchat_token);
+        $this->reader = new \Sabre\XML\Reader();
+        $this->current_playing = '';
     }
 
-    sleep(30);
+    /**
+     * Daemon.  Updates every 30 seconds.
+     */
+    public function run()
+    {
+        while(1)
+        {
+            $currentTime = date('m-d-G:i:00');
+            $this->reader->open('http://www.siriusxm.com/metadata/pdt/en-us/xml/channels/hardattack/timestamp/'.$currentTime);
+            $output = $this->reader->parse();
+
+            $song_title = $output['value'][2]['value'][3]['value'][6]['value'][15]['value'];
+            $song_artist = $output['value'][2]['value'][3]['value'][0]['value'][1]['value'];
+            $now_playing = "Now Playing: $song_title by $song_artist";
+
+            if($this->current_playing == $now_playing)
+            {
+                // Song hasn't changed.  Do nothing.  Log cycle.
+                echo '.';
+            }
+            else
+            {
+                echo PHP_EOL;
+                // New song.  Update current_playing and send alert.
+                $this->current_playing = $now_playing;
+                $this->hc->message_room($this->hipchat_room, 'SiriusXM', $this->current_playing);
+                echo $this->current_playing . PHP_EOL;
+            }
+
+            sleep(30);
+        }
+    }
 }
+
+$service = new SiriusNowPlaying('','dev');
+$service->run();
