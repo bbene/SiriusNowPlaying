9b34603532357873c4c59350b427169ac7e7ca13
Benjamin Benesh <benesh9@gmail.com>
2013-11-27 14:37:53 -0600

    Moves class to its own file. Simplifies main.php file.

--- main.php
+++ main.php
@@ -1,56 +1,9 @@
 <?php
 require_once('vendor/autoload.php');
+require_once('class/SiriusNowPlaying.php');
 
+$my_token = '';
+$my_channel = 'dev';
 
-class SiriusNowPlaying
-{
-    protected $hipchat_room;
-    protected $hc;
-    protected $reader;
-    protected $current_playing;
-
-    public function __construct($hipchat_token, $hipchat_room)
-    {
-        date_default_timezone_set('UTC');
-        $this->hipchat_room = $hipchat_room;
-        $this->hc = new \HipChat\HipChat($hipchat_token);
-        $this->reader = new \Sabre\XML\Reader();
-        $this->current_playing = '';
-    }
-
-    /**
-     * Daemon.  Updates every 30 seconds.
-     */
-    public function run()
-    {
-        while(1)
-        {
-            $currentTime = date('m-d-G:i:00');
-            $this->reader->open('http://www.siriusxm.com/metadata/pdt/en-us/xml/channels/hardattack/timestamp/'.$currentTime);
-            $output = $this->reader->parse();
-
-            $song_title = $output['value'][2]['value'][3]['value'][6]['value'][15]['value'];
-            $song_artist = $output['value'][2]['value'][3]['value'][0]['value'][1]['value'];
-            $now_playing = "Now Playing: $song_title by $song_artist";
-
-            if($this->current_playing == $now_playing)
-            {
-                // Song hasn't changed.  Do nothing.  Log cycle.
-                echo '.';
-            }
-            else
-            {
-                echo PHP_EOL;
-                // New song.  Update current_playing and send alert.
-                $this->current_playing = $now_playing;
-                $this->hc->message_room($this->hipchat_room, 'SiriusXM', $this->current_playing);
-                echo $this->current_playing . PHP_EOL;
-            }
-
-            sleep(30);
-        }
-    }
-}
-
-$service = new SiriusNowPlaying('','dev');
+$service = new SiriusNowPlaying($my_token, $my_channel);
 $service->run();
